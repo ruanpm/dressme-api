9b39229671a46424932b31311ab2330e906d8642
ruanpm <marques.ruan@gmail.com>
2017-05-11 12:56:57 -0300

    add new headers options

--- routes/occasions.js
+++ routes/occasions.js
@@ -6,8 +6,9 @@ var router = express.Router();
 
 //TEST - used to allow access from diferent IP locations
 router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
   next();
 });
 
