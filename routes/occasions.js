5924ff09c01de4a1832a43463b39a87ac4ce0e37
ruanpm <marques.ruan@gmail.com>
2017-05-13 16:35:22 -0300

    add allow origin inside route to test

--- routes/occasions.js
+++ routes/occasions.js
@@ -20,6 +20,11 @@ var router = express.Router();
 		//CREATE NEW OCCASION
 		.post(function(req, res) {
 
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+			
+
 			console.log('Start: NEW OCCASION CREATION');
 
 			var db = firebase.database();
